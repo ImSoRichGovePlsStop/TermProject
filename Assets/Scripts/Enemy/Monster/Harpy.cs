4acb74ff7e3757a7d8b342c5e67ad543f4da7bf3
Sira Sasisakulporn <sira2violin@gmail.com>
2026-03-11 11:33:13 +0700

    Try making Harpy hover

---
+++ Assets/Scripts/Enemy/Monster/Harpy.cs
@@ -0,0 +1,82 @@
+using System;
+using UnityEngine;
+
+public class Harpy : MonoBehaviour
+{
+    enum HarpyState
+    {
+        IdleOnGround,
+        Hover,
+        DiveAttack,
+        Recover
+    }
+
+    HarpyState currentState;
+    private Rigidbody rb;
+    private EnemyController controller;
+    private EnemyMovement movement;
+    private EnemyAttack attack;
+
+    [SerializeField] float hoverHeight = 2f;
+    [SerializeField] float hoverSpeed = 2f;
+    [SerializeField] float hoverAmplitude = 0.3f;
+    [SerializeField] float wakeDistance = 5f;
+    [SerializeField] private Transform player;
+
+    float targetHoverY;
+    private Vector3 startPosition;
+    private float baseY;
+
+    void Awake()
+    {
+        controller = GetComponent<EnemyController>();
+        movement = GetComponent<EnemyMovement>();
+        attack = GetComponent<EnemyAttack>();
+        rb = GetComponent<Rigidbody>();
+    }
+
+    void Start()
+    {
+        startPosition = transform.position;
+        baseY = transform.position.y;
+    }
+
+    void Update()
+    {
+        float distanceToPlayer = Vector3.Distance(transform.position, player.position);
+        
+        switch (currentState)
+        {
+            case HarpyState.IdleOnGround:
+                CheckWakeUp(distanceToPlayer);
+                break;
+
+            case HarpyState.Hover:
+                Hover();
+                break;
+        }
+    }
+
+    void Hover()
+    {
+        float hoverOffset = Mathf.Sin(Time.time * 3f) * hoverAmplitude;
+
+        Vector3 pos = rb.position;
+        pos.y = Mathf.Lerp(pos.y, targetHoverY + hoverOffset, Time.deltaTime * hoverSpeed);
+        rb.MovePosition(pos);
+    }
+
+    void CheckWakeUp(float distance)
+    {
+        if (distance <= wakeDistance)
+        {
+            StartHover();
+        }
+    }
+
+    void StartHover()
+    {
+        currentState = HarpyState.Hover;
+        targetHoverY = transform.position.y + hoverHeight;
+    }
+}
