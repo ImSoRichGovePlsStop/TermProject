befd8b0770d976463cca485aff2e084929cf4399
WsuperwhaleW <whaleahma@gmail.com>
2026-03-13 22:22:38 +0700

    'more_mod'

---
+++ Assets/Scripts/Module/Effects/HpMultiplyModule.cs
@@ -0,0 +1,36 @@
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "Module Effect/HpMultiply")]
+public class HpMultiplyModule : ModuleEffect
+{
+    [Header("Stat per Rarity (Rare - Legendary)")]
+    public float[] baseStatPerRarity = { 0f, 0f, 0f, 0f, 0f };
+    public float levelMultiplier;
+    public int[] cost = { 0, 0, 0, 0, 0 };
+
+    protected override void OnEquip(PlayerStats stats, Rarity rarity, int level, ModuleRuntimeState state)
+    {
+        state.currentStat = GetFinalStat(baseStatPerRarity, levelMultiplier, rarity, level);
+        stats.AddMultiplierModifier(new StatModifier { health = GetEffectiveStat(state) });
+    }
+
+    protected override void OnUnequip(PlayerStats stats, Rarity rarity, int level, ModuleRuntimeState state)
+    {
+        stats.RemoveMultiplierModifier(new StatModifier { health = GetEffectiveStat(state) });
+    }
+
+
+    public override string GetDescription(Rarity rarity, int level, ModuleRuntimeState state)
+    {
+        float baseStat = GetFinalStat(baseStatPerRarity, levelMultiplier, rarity, level);
+        float effective = GetEffectiveStat(state);
+        if (state.totalBuffPercent > 0f)
+            return $"<s>+{baseStat * 100f:F0}%</s> +{effective * 100f:F0}% Hp";
+        return $"+{baseStat * 100f:F0}% Hp";
+    }
+
+    private float GetEffectiveStat(ModuleRuntimeState state)
+    {
+        return state.currentStat * (1f + state.totalBuffPercent);
+    }
+}
