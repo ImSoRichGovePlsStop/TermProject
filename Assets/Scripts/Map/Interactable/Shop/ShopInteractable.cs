04b4ae0872e328feb4acc259ff1225bbf21029bd
poongeta <96272766+poongeta@users.noreply.github.com>
2026-03-16 16:00:54 +0700

    fix: scrolling

--- Assets/Scripts/Map/Interactable/Shop/ShopInteractable.cs
+++ Assets/Scripts/Map/Interactable/Shop/ShopInteractable.cs
@@ -3,11 +3,69 @@ using UnityEngine;
 
 public class ShopInteractable : MonoBehaviour, IInteractable
 {
+    [Header("leave empty to use Randomizer")]
     [SerializeField] private TestModuleEntry[] shopEntries;
+
+    [Header("Randomizer Settings")]
+    [SerializeField] private bool useRandomizer = true;
+    [SerializeField] private int minCount = 3;
+    [SerializeField] private int maxCount = 6;
+    [SerializeField] private float meanCost = 100f;
+    [SerializeField] private float sd = 30f;
+    [SerializeField] private bool allowDuplicates = false;
+
+    [Header("References")]
     [SerializeField] private ShopUI shopUI;
     [SerializeField] private UIManager uiManager;
 
     private readonly HashSet<int> _soldIndices = new HashSet<int>();
+    private TestModuleEntry[] _generatedEntries;
+
+    private void Start()
+    {
+        GenerateEntries();
+    }
+
+    public void SetRandomizerSettings(
+        int minCount,
+        int maxCount,
+        float meanCost,
+        float sd,
+        bool allowDuplicates = false,
+        bool regenerate = true)
+    {
+        this.minCount = minCount;
+        this.maxCount = maxCount;
+        this.meanCost = meanCost;
+        this.sd = sd;
+        this.allowDuplicates = allowDuplicates;
+        this.useRandomizer = true;
+
+        if (regenerate)
+        {
+            _soldIndices.Clear();
+            GenerateEntries();
+        }
+    }
+
+    public void Regenerate()
+    {
+        _soldIndices.Clear();
+        GenerateEntries();
+    }
+
+    private void GenerateEntries()
+    {
+        if (useRandomizer)
+        {
+            var rolled = Randomizer.Roll(minCount, maxCount, meanCost, sd, allowDuplicates);
+            _generatedEntries = rolled.ToArray();
+        }
+        else
+        {
+            _generatedEntries = shopEntries;
+        }
+    }
 
     public void Interact(PlayerController playerController)
     {
@@ -15,7 +73,7 @@ public class ShopInteractable : MonoBehaviour, IInteractable
         if (uiManager == null) { Debug.LogError("[ShopInteractable] UIManager is missing!"); return; }
 
         uiManager.OpenShop(shopUI);
-        shopUI.Populate(shopEntries, _soldIndices, this);
+        shopUI.Populate(_generatedEntries, _soldIndices, this);
     }
 
     public void RegisterSold(int index)
