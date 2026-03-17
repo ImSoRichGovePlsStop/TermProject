01651992af9d497f5a075c7202b7bff33d166e23
poongeta <96272766+poongeta@users.noreply.github.com>
2026-03-17 22:55:16 +0700

    feat: demo map

--- Assets/Scripts/Map/MapGenerator.cs
+++ Assets/Scripts/Map/MapGenerator.cs
@@ -5,6 +5,7 @@ public class MapGenerator : MonoBehaviour
     [Header("Player")]
     public GameObject playerPrefab;
     public GameObject enemiesPrefab;
+    public GameObject lootPrefab;
 
     [Header("room sizes")]
     public Vector2 spawnRoomSize = new Vector2(10f, 10f);
@@ -32,13 +33,25 @@ public class MapGenerator : MonoBehaviour
         GameObject battleRoomObj = CreateRoom("BattleRoom", battleRoomSize, new Vector3(battleRoomX, 0, 0));
         battleRoomObj.AddComponent<BattleRoom>();
         battleRoomObj.GetComponent<BattleRoom>().enemyPrefabs = new GameObject[] { enemiesPrefab };
-
-
+        battleRoomObj.GetComponent<BattleRoom>().lootPrefab = lootPrefab;
 
         BoxCollider trigger = battleRoomObj.AddComponent<BoxCollider>();
         trigger.isTrigger = true;
         trigger.size = new Vector3(battleRoomSize.x, triggerHeight, battleRoomSize.y);
         trigger.center = new Vector3(0, triggerHeight / 2f, 0);
+
+        GameObject battleRoomObj2 = CreateRoom("BattleRoom", battleRoomSize, new Vector3(40, 0, 0));
+        battleRoomObj2.AddComponent<BattleRoom>();
+        battleRoomObj2.GetComponent<BattleRoom>().enemyPrefabs = new GameObject[] { enemiesPrefab };
+        battleRoomObj2.GetComponent<BattleRoom>().lootPrefab = lootPrefab;
+
+        BoxCollider trigger1 = battleRoomObj2.AddComponent<BoxCollider>();
+        trigger1.isTrigger = true;
+        trigger1.size = new Vector3(battleRoomSize.x, triggerHeight, battleRoomSize.y);
+        trigger1.center = new Vector3(0, triggerHeight / 2f, 0);
+
+
+
 
         battleRoomObj.GetComponent<BattleRoom>().SetRoomSize(new Vector3(
                 battleRoomSize.x,
@@ -46,7 +59,13 @@ public class MapGenerator : MonoBehaviour
                 battleRoomSize.y
             ));
 
-      
+        battleRoomObj2.GetComponent<BattleRoom>().SetRoomSize(new Vector3(
+                battleRoomSize.x,
+                triggerHeight,
+                battleRoomSize.y
+            ));
+
+
     }
 
     GameObject CreateRoom(string name, Vector2 size, Vector3 position)
