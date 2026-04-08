5272233a3e17298d44d6af3e1029469205ce012d
WsuperwhaleW <whaleahma@gmail.com>
2026-04-08 10:26:16 +0700

    'balance_fix'

--- Assets/Scripts/Map/Misc/NextFloorPortal.cs
+++ Assets/Scripts/Map/Misc/NextFloorPortal.cs
@@ -5,6 +5,10 @@ public class NextFloorPortal : MonoBehaviour, IInteractable
 {
     public void Interact(PlayerController playerController)
     {
+
+        var player = GameObject.FindWithTag("Player");
+        if (player == null) { Debug.LogWarning("[PlayerSpawner] Player not found!"); return; }
+        player.transform.position = new Vector3(-200f, -75f, -200f);
         int nextScene = SceneManager.GetActiveScene().buildIndex + 0;
         SceneManager.LoadScene(nextScene);
     }
