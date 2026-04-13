0321e983484b2769875ea0d55d0f486033c224b8
poongeta <96272766+poongeta@users.noreply.github.com>
2026-04-14 03:01:02 +0700

    feat:update gamemanager, update build scene to use newmapgen, update text in golden statue

--- Assets/Scripts/Map/Misc/GoldenStatue.cs
+++ Assets/Scripts/Map/Misc/GoldenStatue.cs
@@ -5,8 +5,8 @@ public class GoldenStatue : MonoBehaviour
     [Header("Stat Boost Settings")]
     [Range(0f, 1f)]
     [SerializeField] private float statBoostChance = 0.5f;
-    [SerializeField] private float boostPercentMin = 0.01f; 
-    [SerializeField] private float boostPercentMax = 0.03f; 
+    [SerializeField] private float boostPercentMin = 0.01f;
+    [SerializeField] private float boostPercentMax = 0.03f;
 
     private HealthBase health;
 
@@ -45,26 +45,50 @@ public class GoldenStatue : MonoBehaviour
             case 0:
                 bonus.health = stats.BaseHP * boost;
                 statName = "Max HP";
+                DamageNumberSpawner.Instance?.SpawnMessage(
+                transform.position,
+                $"+{bonus.health:P0} {statName}!",
+                new Color(1f, 0.9f, 0.3f));
                 break;
             case 1:
                 bonus.damage = stats.BaseDMG * boost;
                 statName = "Damage";
+                DamageNumberSpawner.Instance?.SpawnMessage(
+                transform.position,
+                $"+{bonus.damage:P0} {statName}!",
+                new Color(1f, 0.9f, 0.3f));
                 break;
             case 2:
                 bonus.attackSpeed = stats.BaseATKSPD * boost;
                 statName = "Attack Speed";
+                DamageNumberSpawner.Instance?.SpawnMessage(
+                transform.position,
+                $"+{bonus.attackSpeed:P0} {statName}!",
+                new Color(1f, 0.9f, 0.3f));
                 break;
             case 3:
                 bonus.moveSpeed = stats.BaseMOVSPD * boost;
                 statName = "Move Speed";
+                DamageNumberSpawner.Instance?.SpawnMessage(
+                transform.position,
+                $"+{bonus.moveSpeed:P0} {statName}!",
+                new Color(1f, 0.9f, 0.3f));
                 break;
             case 4:
                 bonus.critChance = stats.BaseCrit * boost;
                 statName = "Crit Chance";
+                DamageNumberSpawner.Instance?.SpawnMessage(
+                transform.position,
+                $"+{bonus.critChance:P0} {statName}!",
+                new Color(1f, 0.9f, 0.3f));
                 break;
             case 5:
                 bonus.critDamage = stats.BaseCritDMG * boost;
                 statName = "Crit Damage";
+                DamageNumberSpawner.Instance?.SpawnMessage(
+                transform.position,
+                $"+{bonus.critDamage:P0} {statName}!",
+                new Color(1f, 0.9f, 0.3f));
                 break;
             default:
                 statName = "Stat";
@@ -73,11 +97,7 @@ public class GoldenStatue : MonoBehaviour
 
         stats.AddFlatModifier(bonus);
 
-        DamageNumberSpawner.Instance?.SpawnMessage(
-            transform.position,
-            $"+{boost:P0} {statName}!",
-            new Color(1f, 0.9f, 0.3f)
-        );
+
 
         Destroy(gameObject);
     }
