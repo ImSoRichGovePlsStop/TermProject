2ecec069d0a0b22480ce7ce0db75256a259941c3
poongeta <96272766+poongeta@users.noreply.github.com>
2026-04-15 18:29:59 +0700

    fix: minor code

--- Assets/Scripts/Map/Misc/StartPortal.cs
+++ Assets/Scripts/Map/Misc/StartPortal.cs
@@ -6,10 +6,6 @@ public class StartPortal : MonoBehaviour
     private void OnTriggerEnter(Collider other)
     {
         if (!other.gameObject.CompareTag("Player")) return;
-        int nextScene = SceneManager.GetActiveScene().buildIndex + 1;
-        if (nextScene < SceneManager.sceneCountInBuildSettings)
-            SceneManager.LoadScene(nextScene);
-        else
-            Debug.LogWarning("[StartPortal] No next scene in build settings.");
+        SceneManager.LoadScene(2);
     }
 }
